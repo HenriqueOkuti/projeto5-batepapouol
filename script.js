05bac8f30d135ab8ebed6a097cb2093a64711ee7
HenriqueOkuti <henrique.krastins@gmail.com>
2022-06-22 21:13:28 -0300

    Insert chatbox

--- script.js
+++ script.js
@@ -6,12 +6,41 @@
     AUXILIAR FUNCTIONS
 ========================*/
 
+function load_messages() {
+  const promise = axios.get(
+    "https://mock-api.driven.com.br/api/v6/uol/messages"
+  );
+  promise.then(messagelog);
+}
+
 /*========================
     MAIN FUNCTIONS
 ========================*/
 
+function chat_start() {
+  load_messages();
 
-function messagelog() {
-    
+  setInterval(load_messages, 3000);
 }
 
+function messagelog(resposta) {
+  const messages_box = document.querySelector(".messages_box");
+
+  messages_box.innerHTML = "";
+
+  for (let i = 0; i < resposta.data.length; i++) {
+    const message = resposta.data[i];
+
+    if (message.type == "message") {
+      messages_box.innerHTML += `
+                <div class="public-chat">
+                <span class="time">(${message.time})</span>
+                <strong>${message.from}</strong>
+                <span> para </span>
+                <strong>${message.to}: </strong>
+                <span>${message.text}</span>
+                </div>
+                `;
+    }
+  }
+}
