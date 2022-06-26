dfdaad58a817c8e2e0a4ac4a30acdd5d2a093b70
HenriqueOkuti <henrique.krastins@gmail.com>
2022-06-26 19:26:35 -0300

    Bug fixes, also allows the user to recieve private messages, this will be the deploy version since there is mostly the bonuses to be done

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let global_aux = 0;
     AUXILIAR FUNCTIONS
 ========================*/
 
+//Loads messages from the server
 function load_messages() {
   const promise = axios.get(
     "https://mock-api.driven.com.br/api/v6/uol/messages"
@@ -19,26 +20,32 @@ function load_messages() {
   promise.catch(reset);
 }
 
+//Reloads page
 function reset() {
   window.location.reload();
 }
 
-function private_msg_check(message) {}
+function private_msg_check(message) {
+  if (message.to === 'Todos' || message.to !== name) {
+    return false;
+  }
+  return true;
+}
 
-function login_user(error) {
+//Logs user in the chat
+function login_user() {
   const promise = axios.post(
     "https://mock-api.driven.com.br/api/v6/uol/participants",
     { name: name }
   );
 
+  //If error then resets login
   promise.catch(reset_login);
-
-
 }
 
+//Recieves an error code, if duplicated user then restarts the procedure via chat_start()
 function reset_login(error) {
   error_code = (error.response.status);
-  console.log(typeof(error_code));
   if (error_code == 400) {
     alert(`Usuário já presente! Tente novamente com um nome diferente de ${name}`);
     name = undefined;
@@ -46,6 +53,7 @@ function reset_login(error) {
   }
 }
 
+//Keeps user logged in
 function keep_online() {
   const promise = axios.post(
     "https://mock-api.driven.com.br/api/v6/uol/status",
@@ -65,7 +73,10 @@ function chat_start() {
   login_user();
   load_messages();
 
+  //Keeps updating the messages
   setInterval(load_messages, 3000);
+
+  //Keeps user online
   setInterval(keep_online, 5000);
 }
 
@@ -77,6 +88,8 @@ function messagelog(resposta) {
   for (let i = 0; i < resposta.data.length; i++) {
     const message = resposta.data[i];
 
+
+    //Public messages
     if (message.type == "message") {
       messages_box.innerHTML += `
         <div class="public-chat chat">
@@ -89,6 +102,7 @@ function messagelog(resposta) {
         `;
     }
 
+    //Status messages
     if (message.type == "status") {
       messages_box.innerHTML += `
         <div class="status chat">
@@ -99,6 +113,8 @@ function messagelog(resposta) {
         `;
     }
 
+    //Private messages
+      //if TRUE then user is able to view private message
     if (private_msg_check(message)) {
       messages_box.innerHTML += `
       <div class="private-chat chat">
@@ -112,10 +128,11 @@ function messagelog(resposta) {
     }
   }
 
+  //message buffer for scroll into view
   const last_msg = document.querySelector(".messages_box .chat:last-child");
   if (global_aux == 0) {
     msg_buffer.push(last_msg);
-    msg_buffer.push(last_msg);
+    msg_buffer.push(last_msg);  //Somehow it works when i do a double push, witchcraft perhaps?
     global_aux++;
   }
   msg_buffer.push(last_msg);
@@ -123,10 +140,12 @@ function messagelog(resposta) {
   let penultimate_message = msg_buffer[msg_buffer.length - 2];
   let last_message = msg_buffer[msg_buffer.length - 1];
 
+  //Actual buffer comparison
   if (last_message.innerHTML !== penultimate_message.innerHTML) {
     last_msg.scrollIntoView();
   }
 
+  //Buffer reset so that i does not grow into infinity
   if (msg_buffer.length > 3) {
     msg_buffer = [];
 
@@ -137,6 +156,13 @@ function messagelog(resposta) {
 
 function send_message() {
   const text = document.querySelector(".text_message").value;
+
+  //Prevents error with blank message
+  if (text == "") {
+    return;
+  }
+
+  //Writes message as an object, without ability to select a person to send or type of message (private or public)
   const text_obj = {
     from: name,
     to: "Todos",
@@ -144,11 +170,14 @@ function send_message() {
     type: "message",
   };
 
+  //Sends the message object to the server and resets the previous text from the screen
   document.querySelector(".text_message").value = "";
   const promise = axios.post(
     "https://mock-api.driven.com.br/api/v6/uol/messages",
     text_obj
   );
+
+  //loads messages from server
   promise.then(load_messages);
   promise.catch(reset);
 }
